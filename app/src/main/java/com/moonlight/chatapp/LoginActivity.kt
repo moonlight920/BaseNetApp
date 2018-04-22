e32a71ea5c1a39c687ae5d6bab98aa063a409390
songyifeng <songyifeng@haodou.com>
2018-04-23 01:31:40 +0800

    commit tablayout

---
+++ app/src/main/java/com/moonlight/chatapp/LoginActivity.kt
@@ -0,0 +1,60 @@
+package com.moonlight.chatapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        var editTextUsername:EditText = til_username.editText!!
+        til_username.hint = "UserName"
+        editTextUsername.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length > 4) {
+                    til_username.error = "UserName error"
+                    til_username.isErrorEnabled = true
+                } else {
+                    til_username.isErrorEnabled = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+        var editTextPwd:EditText = til_pwd.editText!!
+        til_pwd.hint = "Password"
+        editTextPwd.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length > 4) {
+                    til_pwd.error = "Password error"
+                    til_pwd.isErrorEnabled = true
+                } else {
+                    til_pwd.isErrorEnabled = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+    }
+}
